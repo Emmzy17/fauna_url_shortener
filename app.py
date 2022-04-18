bfadedc0f27d62d38b431355e5c219be9f6b8bda
Emmzy17 <emmanueladjerry@gmail.com>
2022-04-18 10:25:02 +0100

    url_shorterner with fauna

---
+++ app.py
@@ -0,0 +1,46 @@
+import string
+import random
+
+from flask import Flask, jsonify, request, abort, redirect
+from faunadb import query as q
+from faunadb.objects import Ref
+from faunadb.client import FaunaClient
+
+app = Flask(__name__)
+client = FaunaClient(secret='fnAEkcYi5fAAQuc4Tcg8cVOVperOr6e2B8iOVner', domain=	'db.us.fauna.com', scheme='https')
+
+
+@app.route('/')
+def hello():
+    return 'hello world'
+
+
+
+def generate_identifier(n=6):
+    identifier = ''
+    for i in range(6):
+        identifier+=random.choice(string.ascii_letters)
+    return identifier
+
+@app.route('/generate/<path:address>/')
+def generate(address):
+    identifier = generate_identifier()
+    client.query(q.create(q.collection('urls'),
+    {            'data':{
+                'identifier':identifier, 
+                'url': address, 
+            }
+        }
+        ))
+    shortened_url = request.host_url + identifier
+    return jsonify({'identifier': identifier, 'shortened_url': shortened_url})
+@app.route('/<string:identifier>')
+def fetch_original(identifier):
+    try:
+        url = client.query(q.get(q.match(q.index('urls_by_identifier'), identifier)))
+    except:
+        abort(404)
+    return redirect(url['data']['url'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
